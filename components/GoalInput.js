f1d0045f3731945cf76b64cbd3734ed3ff199486
Afrizal Bagas <afrizalbagas.03@gmail.com>
2025-04-26 13:33:28 +0700

    fix

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -11,8 +11,7 @@ import {
 import ListImage from "../assets/list.png";
 
 function GoalInput(props) {
-  const [enteredGoalText, setEnteredGoalText] = useState("");
-
+  const [enteredGoalText, setEnteredGoalText] = useState();
   function handleSearchInput(enteredText) {
     setEnteredGoalText(enteredText);
   }
